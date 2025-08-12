8c1d9a1f4e3a9d045737507339c8b8097c03799e
arilbenzema <shahfit17@gmail.com>
2025-08-12 20:44:53 +0800

    update reources & routes

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,17 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
+
+
+Route::get('/tentang-blog', function () {
+    return view('about');
+})->name('about');
+
+Route::get('/hubungi-kami', function () {
+    return view('contact');
+})->name('contact');
+
+Route::get('/produk', function () {
+    return view('product');
+})->name('product');
